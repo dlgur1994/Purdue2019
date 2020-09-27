dc78cacbf3bb8f5bbd3be5955b5bfe53be5c4b63
Brian <sydbne17@gmail.com>
2020-09-27 21:03:16 +0900

    file location changed

---
+++ APRS/Distance Test/Arduino/No_Radio_Shield/No_Radio_Shield.ino
@@ -0,0 +1,60 @@
+/* trackuino copyright (C) 2010  EA5HAV Javi
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// Trackuino custom libs
+#include "config.h"
+#include "afsk_avr.h"
+#include "aprs.h"
+#include "pin.h"
+#include "power.h"
+#include <Arduino.h>
+
+// Module variables
+static int32_t next_aprs = 3000;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  afsk_setup();
+
+}
+int count = 0;
+void loop()
+{
+  if ((int32_t) (millis() - next_aprs) >= 0) {
+    // Time for another APRS frame
+    String a = "abc";
+    String b = "cde";
+    String c = a + b + String(count);
+    count++;
+    count %= 10;
+    aprs_send("##" + c);
+    next_aprs += APRS_PERIOD * 1000L;
+    while (afsk_flush()) {
+      power_save();
+    }
+
+  } else {
+    // Discard GPS data received during sleep window
+    while (Serial.available()) {
+      Serial.read();
+    }
+  }
+
+  power_save(); // Incoming GPS data or interrupts will wake us up
+}
